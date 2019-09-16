9a19b3885dda6c1b8216d8b4a48eecdc6fd14a86
MagicBear <mb@bilibili.com>
2019-09-16 20:11:41 +0800

    Support specified msg lottery

--- lottery.js
+++ lottery.js
@@ -1,5 +1,5 @@
-var getReplyCount = 10;
-var avid = 57733423;
+var getReplyCount = 1;
+var avid = 67519685;
 
 function shuffle(a) {
     var j, x, i;
@@ -12,17 +12,19 @@ function shuffle(a) {
     return a;
 }
 var uidLists = {};
+var uidMsgList = {};
 $.getJSON("/x/web/replies?order=ctime&filter=-1&is_hidden=0&type=1&oid="+avid+"&pn=1&ps=1000", function(data) {
     for (var i = 0; i < data.data.length; i++)
     {
-        if (data.data[i].relation == 2)
+        if (data.data[i].relation == 2 && data.data[i].message.indexOf("我") != -1 && data.data[i].message.indexOf("要") != -1)
         {
             uidLists[data.data[i].mid] = data.data[i].replier;
+            uidMsgList[data.data[i].mid] = data.data[i].message;
         }
     }
     var shuffleList = shuffle(Object.keys(uidLists));
     for (var i = 0; i < getReplyCount; i++)
     {
-        console.log("恭喜@"+uidLists[shuffleList[i]]+" 【"+shuffleList[i]+"】中奖");
+        console.log("恭喜@"+uidLists[shuffleList[i]]+" 【"+shuffleList[i]+"】中奖 => "+uidMsgList[shuffleList[i]]);
     }
 });
